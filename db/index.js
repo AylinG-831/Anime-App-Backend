225e5a9d447a8346d87a5c0393f8272a2df703e4
aylin <asg195@scarletmail.rutgers.edu>
2022-04-23 03:28:32 -0400

    changes to connection

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/animeListDatabase';
 
-let chooseChaos = "mongodb+srv://kobeni:EextNwD4yUce69cu@cluster0.thwnq.mongodb.net/animeListDatabase?retryWrites=true&w=majority";
+let chooseChaos = process.env.Chaos
 
 mongoose.connect(chooseChaos, { useUnifiedTopology: true, useNewUrlParser: true}).then( () => {
     console.log("Successfully connected to MongoDB");
